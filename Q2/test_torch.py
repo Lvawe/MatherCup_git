2bde7b7bf8c848534488c173c5c8c0620f40e7dd
Qianrui Lv <lvqr030225@gmail.com>
2023-05-23 21:07:25 +0800

    test

--- Q2/test_torch.py
+++ Q2/test_torch.py
@@ -1,2 +1,8 @@
 import torch
 print(torch.version.cuda)
+if torch.cuda.is_available():
+    print("GPU可用")
+else:
+    print("GPU不可用")
+device_name = torch.cuda.get_device_name(torch.cuda.current_device())
+print("当前使用的GPU设备：", device_name)
